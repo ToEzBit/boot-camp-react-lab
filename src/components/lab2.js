26a0ae3fa398429e4f2aac0e73e1594b5bae8671
ToEzBit <jirayut52328172@hotmail.com>
2022-04-15 23:40:43 +0700

    lab2 succes

---
+++ src/components/lab2.js
@@ -0,0 +1,24 @@
+const Lab2 = () => {
+  return (
+    <div>
+      <div>
+        <label>Province :</label>
+        <select>
+          <option>Bangkok</option>
+          <option>Nontaburi</option>
+          <option>Pathum</option>
+        </select>
+      </div>
+      <div>
+        <label>District :</label>
+        <select>
+          <option>District 1</option>
+          <option>District 2</option>
+          <option>District 3</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Lab2;
