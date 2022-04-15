2d6f46e195206c078b5e4131a4102f88346a3736
ToEzBit <jirayut52328172@hotmail.com>
2022-04-15 23:25:40 +0700

    lab sucess

---
+++ src/components/lab1.js
@@ -0,0 +1,15 @@
+const Lab1 = () => {
+  return (
+    <div>
+      <h1>Hello My Friend</h1>
+      <p>Friend List</p>
+      <ul>
+        <li>John</li>
+        <li>Jack</li>
+        <li>Jane</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Lab1;
