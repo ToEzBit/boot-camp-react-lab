7c74e7927bebc3f5ce1a8070ab4b436797bf6749
ToEzBit <jirayut52328172@hotmail.com>
2022-04-16 16:40:25 +0700

    lab4 sucees

---
+++ src/components/lab4.js
@@ -0,0 +1,21 @@
+const Lab4 = () => {
+  return (
+    <div>
+      <button>Add</button>
+      <div>
+        <label>
+          <del>Task1</del>
+        </label>
+        <button>Edit</button>
+        <button>Del</button>
+      </div>
+      <div>
+        <label>Task2</label>
+        <button>Edit</button>
+        <button>Del</button>
+      </div>
+    </div>
+  );
+};
+
+export default Lab4;
