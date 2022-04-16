68319fc09c63bbfb6c05ce8efa21aaacd1086210
ToEzBit <jirayut52328172@hotmail.com>
2022-04-16 16:29:12 +0700

    lab3 succes

---
+++ src/components/lab3.js
@@ -0,0 +1,14 @@
+const Lab3 = () => {
+  return (
+    <div>
+      <div>
+        <button>+</button>
+        <span>0</span>
+        <button>-</button>
+        <button>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default Lab3;
